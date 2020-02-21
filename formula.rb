69b7cf6207c4a54ce462a4151823474c4dbe76c8
yuz <re.yuz77777@gmail.com>
2020-02-21 21:53:29 +0900

    Initial commit

---
+++ formula.rb
@@ -0,0 +1,10 @@
+class PKGO < Formula
+  desc "Open the document for Go package by CLI"
+  url "https://github.com/yuzuy/pkgo/releases/download/v0.1.0/pkgo"
+  sha256 "51b0bbd05e889057ac9a4584f820b4da765f2ce2a49ee26c6ffdd55b72362997"
+
+  def install
+    bin.install "pkgo"
+  end
+end
+
