d0f1b6869bd6b1431625153af2622be272857986
yuz <re.yuz77777@gmail.com>
2020-02-21 22:00:41 +0900

    fix

--- pkgo.rb
+++ pkgo.rb
@@ -1,4 +1,4 @@
-class PKGO < Formula
+class Pogo < Formula
   desc "Open the document for Go package by CLI"
   url "https://github.com/yuzuy/pkgo/releases/download/v0.1.0/pkgo"
   sha256 "51b0bbd05e889057ac9a4584f820b4da765f2ce2a49ee26c6ffdd55b72362997"
